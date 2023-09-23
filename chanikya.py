857ba12e6445567cb7d0415410c07fa434c19a22
chanu <144309426+chanu@users.noreply.github.com>
2023-09-23 17:43:58 +0530

    Add files via upload

--- chanikya.py
+++ chanikya.py
@@ -1,35 +1,9 @@
-import os
-import pickle
-#os.mkdir("c:\\binaryset")
-os.chdir("c:\\binaryset")
-
-file =open("c:\\binaryset\\binary.dat","ab+")
-
-binaryset={"one","two"}
-
-binarylist=["one","two","three","four"]
-
-binaryData={1:"chanu",'company':"Clariont"}
-
-# pickle.dump(binaryset,file)
-# file.seek(0,0)
-# output=pickle.load(file)
-# print("output :",output)
-# print("the cursor position is :",file.tell())
-
-
-# pickle.dump(binaryData,file)
-# file.seek(28,0)
-# output=pickle.load(file)
-# print("output :",output)
-# print("the cursor position is :",file.tell())
-
-# pickle.dump(binarylist,file)
-# file.seek(75,0)
-# output=pickle.load(file)
-# print("output :",file.tell())
-# next position is 118 :
-
-file.seek(0,0)
-output=pickle.load(file)
-print("output :",output)
+L=["X", 20, "Y", 10, "Z", 30]
+CNT=0
+ST =" "
+INC=0
+for C in range(1,6,2):
+ CNT=CNT +C
+ ST=ST +L[C-1]+"@"
+ INC= INC +L[C]
+ print (CNT, INC, ST)
